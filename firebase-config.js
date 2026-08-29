7028f88a385771d5c33136e155b36505d1168f05
ljhamilleonardo-svg <ljhamilleonardo@gmail.com>
2026-08-28 20:27:02 -0400

    Add files via upload
    Primera versión del test vocacional

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+/* Configuración real de Firebase para el proyecto test-vocacional-itsa */
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCZygU2g-1xJEg5Vmdn9EXHp7MvhR0GP7U",
+  authDomain: "test-vocacional-itsa.firebaseapp.com",
+  databaseURL: "https://test-vocacional-itsa-default-rtdb.firebaseio.com",
+  projectId: "test-vocacional-itsa",
+  storageBucket: "test-vocacional-itsa.firebasestorage.app",
+  messagingSenderId: "604432620910",
+  appId: "1:604432620910:web:1cb67d24f64aab83e4757f"
+};
+
+firebase.initializeApp(firebaseConfig);
